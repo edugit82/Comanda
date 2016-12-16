72447fe8f36a7197c6dcc233cc95896768d9feb8
Eduardo correa dos Santos <eduardocorrea82@hotmail.com>
2016-12-16 16:15:46 -0200

    Inicio 16/12/2016 - 16:16

---
+++ Comanda.Testes/TesteUnitarioGeral.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Comanda.Testes
+{
+    [TestClass]
+    public class TesteUnitarioGeral
+    {
+        [TestMethod]
+        public void Teste01()
+        {
+
+        }
+    }
+}
